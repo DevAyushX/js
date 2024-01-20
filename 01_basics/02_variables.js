0a3a18c64f1f213120805e9f98ede6b2d35538e0
Ayush <kmrayus@gmail.com>
2024-01-20 12:30:39 +0000

    Define variables and contraints

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,13 @@
+const accountId = 5959
+let accountEmail = "kmr@gmail.com"
+var accountPassword = "123"
+let accountCity = "Patna"
+let accountState;
+
+
+accountEmail = "hnhn@hn.com"
+accountPassword = "567667"
+accountCity = "Bhubaneswar"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+``
